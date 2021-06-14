6df86b5b790d4c4f89cd4270d84151ef7c8a71d6
Denis Lomanov <lomanovdenis97@gmail.com>
2021-06-14 16:56:45 +0700

    init commit

---
+++ NetBarcodeTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using NetBarcode;
+
+namespace NetBarcodeTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var barcode = new Barcode("543534"); // default: Code128
+            string image = barcode.GetBase64Image();
+            File.WriteAllText("./image-base64.txt", image);
+        }
+    }
+}
